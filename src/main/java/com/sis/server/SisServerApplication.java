eab4f93db364369cf6a5610698a72f4582ed5339
Baran Buzluk <baranbuzluk@gmail.com>
2024-10-20 14:29:18 +0300

    Initialize sis-server spring project

---
+++ src/main/java/com/sis/server/SisServerApplication.java
@@ -0,0 +1,13 @@
+package com.sis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisServerApplication.class, args);
+	}
+
+}
