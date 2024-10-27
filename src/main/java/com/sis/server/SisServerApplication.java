ef8eec16d21f122aa240a2c0f7ffd741d9c9cc91
Baran Buzluk <baranbuzluk@gmail.com>
2024-10-27 20:43:25 +0300

    Reformat all files

--- src/main/java/com/sis/server/SisServerApplication.java
+++ src/main/java/com/sis/server/SisServerApplication.java
@@ -3,11 +3,10 @@ package com.sis.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sis.server")
 public class SisServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SisServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SisServerApplication.class, args);
+  }
 }
